1bb07b4368067db01ea8fcec3de54c616e353334
ColinMitchellGit <colinmitch.cs@gmail.com>
2022-03-19 21:10:57 -0400

    Added initial create recipe, delete recipe, and edit recipe endpoints

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.post('/api/verify', async (req, res, next) =>
     const { username, password, firstName, lastName, profilePic, email } = req.body;
 
     // Creating user object to insert into collection.
-     var user = {
+    var user = {
         username:username, password:password, firstName:firstName,
         lastName:lastName, profilePic:profilePic, email:email
     };
@@ -134,6 +134,111 @@ app.post('/api/verify', async (req, res, next) =>
             res.status(200).json(ret);
         }
     });
+});
+
+// CREATE RECIPE ENDPOINT
+app.post('/api/createRecipe', async (req, res, next) =>
+{
+    // Input  = recipe name, description, country, text, userID, coordinates, picLink
+    const { name, description, country, text, id, coordinates, picLink } = req.body;
+
+    // Creating user object to insert into collection.
+     var recipe = {
+        name:name, desc:description, picURL:picLink,
+        country:country, recipeText:text, creator:id, coordinate: coordinates
+    };
+
+    const db = client.db();
+
+	// Add recipe to collection
+    db.collection("recipes").insertOne(recipe, function(err, res) {
+        if (err)
+        {
+            let ret = { error:'Error while creating recipe.' };
+            res.status(400).json(ret);
+        }
+        else
+        {
+			// After successfully creating the recipe we add it to the user's created array.
+			db.collection("users").updateOne(
+				{ _id: id },
+				{ $push: { created: recipe._id }}
+			)
+
+			const results = await db.collection('countries').find({name:country}).toArray();
+
+			// If the country already exists we just append the recipe to the recipes array.
+			if ( results.length > 0 )
+			{
+				db.collection("countries").updateOne(
+			        { _id: results[0]._id },
+			        { $push: { recipes: recipe._id } }
+			    );
+			}
+			// If the country doesn't already exist, we add it with the recipe.
+			else
+			{
+				var country = { name:country, recipes:[recipe._id] };
+
+				db.collection("countries").insertOne(country);
+			}
+
+            let ret = { error:'' };
+            res.status(200).json(ret);
+        }
+    });
+});
+
+// DELETE RECIPE ENDPOINT
+app.post('/api/deleteRecipe', async (req, res, next) =>
+{
+    // Input = recipeID, country name
+    const { recipeID, country } = req.body;
+
+    const db = client.db();
+
+    // Delete recipe from recipes collection.
+    db.collection("recipes").deleteOne({ _id:recipeID });
+
+	// Removing recipe from the country's list of recipes.
+	db.collection("countries").updateOne(
+		{ name:country },
+		{
+			$pull: { recipes:recipeID }
+		}
+	);
+
+	// Removing the recipe from both the favorited and liked arrays for each user
+	// VERY INEFFICIENT GOTTA FIND A BETTER WAY
+	db.collection("users").updateMany(
+		{ },
+		{
+			$pull: { favorites:recipeID, liked:recipeID }
+		}
+	);
+
+    // Okay status
+    res.status(200);
+});
+
+// EDIT RECIPE ENDPOINT
+app.post('/api/editRecipe', async (req, res, next) =>
+{
+    // Input = recipe name, description, picture link, text, and Recipe ID.
+    const { name, description, picLink, text, recipeID } = req.body;
+
+    const db = client.db();
+
+    // Update recipe with new information.
+    db.collection("recipes").updateOne(
+        { _id: recipeID },
+		{
+			$set: { name:name, desc:description, picURL:picLink, recipeText:text }
+		}
+    );
+
+    // Okay status
+    res.status(200);
 });
 
 // ADD FAVORITE ENDPOINT
@@ -189,7 +294,7 @@ app.post('/api/addLike/', async (req, res, next) =>
     const db = client.db();
 
     // Update like count for recipe
-    db.recipes.udpateOne(
+    db.recipes.updateOne(
         { _id: recipeID },
         { $inc: { likes: 1 } }
     );
