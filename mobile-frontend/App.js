7e2c65a0b05c5937ecdee94a3e785111413c2348
Nathan Kofoed <natekofoed@gmail.com>
2022-03-26 14:09:56 -0400

    Crying inside

--- mobile-frontend/App.js
+++ mobile-frontend/App.js
@@ -8,7 +8,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Header, HeaderBackground} from '@react-navigation/elements';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+
 const Stack = createNativeStackNavigator();
+
+function landingPage()
+{
+  return(
+    <Stack.Navigator>
+        <Stack.Screen name= "Log In" component= {LogIn} options={{headerShown: false}} />
+        <Stack.Screen name= "Sign Up" component = {SignUpScreen} options= {{title: "" , headerTransparent: true}} />
+      </Stack.Navigator>
+  )
+}
 
 
 export default function App() {
---
+++ mobile-frontend/pages/Main.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {TouchableOpacity, ScrollView} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native-picasso';
+import { Feather, Ionicons } from '@expo/vector-icons';
+import Input from '../components/Input';
+
+export default function Main (props)
+{
+    <SafeAreaView>
+        Testing
+    </SafeAreaView>
+}
