fa88ec1a560395f5e260684b9e94d1000557ded8
Prexzer <60827526+Prexzer@users.noreply.github.com>
2022-03-29 16:19:26 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
-import { App, Register } from "./App";
+import Login from "./Login";
+import Register from "./Register"
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <Register />
+    <StrictMode>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/register" >
+          <Register />
+        </Route>
+        <Route path="/landing" >
+          <Register />
+        </Route>
+      <Redirect to = "/" />
+      </Switch>
+    </Router>
   </StrictMode>,
   rootElement
 );
