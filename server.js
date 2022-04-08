cf29c3f482abcf781f4de66a9289a21e699d0735
Alex Peterson <alexpeterson0804@gmail.com>
2022-04-07 23:33:04 -0400

    Re-add production building

--- server.js
+++ server.js
@@ -585,3 +585,12 @@ app.listen(PORT, () =>
 {
     console.log('Server is listening on port ' + PORT);
 });
+
+if (process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('web-frontend/build'));
+    app.get('*', (req, res) =>
+    {
+        res.sendFile(path.resolve(__dirname, 'web-frontend', 'build', 'index.html'));
+    });
+}
