ef57260ee8f44410b90c5f3a46ed7fcf523b2359
Alex Peterson <alexpeterson0804@gmail.com>
2022-04-07 23:57:26 -0400

    Moved emails back to server.js

--- server.js
+++ server.js
@@ -11,8 +11,26 @@ const bcrypt = require('bcryptjs'),
 
 const PORT = process.env.PORT || 5000;
 
-import { email } from './email.js';
-
+const email = {
+    mobile: `<html>
+              <div style="margin-top:50px;text-align: center; font-family: Arial;" container>
+                <h1> Welcome! </h1>
+                <p style="margin-bottom:30px;"> Here's your verification code!</p>
+                <a clicktracking="off" href="verifyLink" style="background:blue;color:white;padding:10px;margin:200px;border-radius:5px;text-decoration:none;">code</a>
+              </div>
+            </html>`,
+    web: `<html>
+            <div style="margin-top:50px;text-align: center; font-family: Arial;" container>
+              <h1> Welcome! </h1>
+              <p style="margin-bottom:30px;"> Verify your account by clicking the following link:</p>
+              <a clicktracking="off" href="verifyLink" style="background:blue;color:white;padding:10px;margin:200px;border-radius:5px;text-decoration:none;">VERIFY</a>
+            </div>
+            <div style="margin-top:50px;text-align: center; font-family: Arial; font-size: 13px">
+              <p> Alternatively, paste this link into your browser.  </p>
+              <a>verifyLink</a>
+            </div>
+          </html>`
+};
 
 const senderEmail = process.env.SENDER_EMAIL;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
